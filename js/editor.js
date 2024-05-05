a446c6e81cfdced8432d53ed25ffe46b1df77109
Eazed <Eazed_Yu@outlook.com>
2024-05-05 11:35:21 +0800

    不再使用require.js管理markdown

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,9 @@
 var editor;
+
 window.addEventListener('load', function () {
-    require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs' }});
+    require.config({
+        paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs' }
+    });
     require(['vs/editor/editor.main'], function () {
         editor = monaco.editor.create(document.getElementById('editor'), {
             value: [
@@ -13,12 +16,15 @@ window.addEventListener('load', function () {
             language: 'cpp',
             theme: 'vs-dark'
         });
+
         document.getElementById('languageSelector').addEventListener('change', function (e) {
             monaco.editor.setModelLanguage(editor.getModel(), e.target.value);
         });
+
         document.getElementById('themeSelector').addEventListener('change', function (e) {
             editor.updateOptions({ theme: e.target.value });
         });
+
         document.getElementById('fontSizeSelector').addEventListener('change', function (e) {
             editor.updateOptions({ fontSize: e.target.value });
         });
